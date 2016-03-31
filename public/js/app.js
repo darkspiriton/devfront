7a51b40b0f98b617cb49eebabe88ab4a3696cc60
Cesar Moreno <cesar200526@gmail.com>
2016-03-31 12:15:42 -0500

    Prueba de Angular with Tabs

--- public/js/app.js
+++ public/js/app.js
@@ -6,3 +6,9 @@ app.controller('appController',function(){
     languages: ['AngularJS','Stylus','Jade']
   };
 });
+app.controller('TabsController',function (){
+  this.tab = 1;
+  this.selectTab = function (tab) {
+    this.tab=tab;
+  }
+});
